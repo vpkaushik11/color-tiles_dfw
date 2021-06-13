f7536ffb431534d75bd9828cbab3c13df3042c59
vpkaushik11 <83803726+vpkaushik11@users.noreply.github.com>
2021-06-13 18:57:25 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,62 +1,21 @@
-const main = document.querySelectorAll('#main');
-console.log(main);
-const peace = document.querySelector('table.play');
+let cell = document.querySelectorAll('.main');
 //console.log(peace);
-const btn = document.querySelector('button.rst');
+const btn = document.querySelector('button');
 
 
-/* let blank = main[Math.floor(Math.random()*24)];
-let blank2 = main[Math.floor(Math.random()*24)];
-console.log(blank);
-console.log(blank2);
-// swapping func
-if (blank!==blank2){
-    let temp;
-    temp = blank.textContent;
-    blank.textContent=blank2.textContent;
-    blank2.textContent=temp;
-    console.log(blank.textContent);
-    console.log(blank2.textContent);
-}
-*/
-let i = Math.floor(Math.random()*24);
-let j = Math.floor(Math.random()*24);
-// this step make one random cell blank
 btn.onclick = function(){
-    main[i].classList.add('blank');
-//swap text content of 2 cell
-};
+    let num = Math.floor(Math.random()*24);
+    cell[num].classList.add('blank')
+    for (let i = 0; i < cell.length; i++) {
+            if(i==num+1||i==num-1||i==num+5||i==num-5){
+            cell[i].onclick = function(){
+                let temp  = cell[num].textContent;
+                cell[num].textContent = cell[i].textContent;
+                cell[i].textContent = temp;
 
-function swap (){
-    console.log(main[i]);
-    console.log(main[j]);
-    let temp = main[i].textContent;
-    main[i].textContent=main[j].textContent;
-    main[j].textContent=temp;
-    main[i].classList.remove('blank');
-    main[j].classList.add('blank');
-    let t = i;
-    i = j;
-    j = t; 
-}
-main[i].onclick = swap;
-
-
-const troy = document.querySelector('.try');
-/*let b = 10;
-while(b > 0){
-    main[i+1].onclick = function(){
-        j = i+1;
-        main[j].classList.add('blank');
-        main[i].classList.remove('blank');
-        let temp = main[i].textContent;
-        main[i].textContent = main[j].textContent;
-        main[j].textContent = temp;
-        let t = i;
-        i=j;
-        j=t;
-        b--;
-
+                cell[num].classList.remove('blank');
+                cell[i].classList.add('blank');
+            } num = i;
+        }
     }
 }
-*/
