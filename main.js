0d56cf8bcbba337ac57b6ac4a012f1702bf27302
vpkaushik11 <83803726+vpkaushik11@users.noreply.github.com>
2021-06-21 20:00:42 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
-// stuff begins here
-const btn = document.querySelector('button');
+const btn = document.querySelector('button.rst');
 
 let hex=["red","green","blue","orange","violet","yellow"]; //colors
 let hexCount1 = [0,0,0,0,0,0]; // to prevent the colors from repeating
@@ -57,10 +56,13 @@ function swap(x1,x2){
     let z = document.getElementById(x1).style.backgroundColor;
     document.getElementById(x1).style.backgroundColor = document.getElementById(x2).style.backgroundColor;
     document.getElementById(x2).style.backgroundColor = z;
+    move++;
 }
 
 // selection of cells 
 function clickyboy (i,j){
+    if(move==0)
+      start();
     let bae = document.getElementById("main"+i+j)
     let c = bae.className;
     console.log(bae);
@@ -86,5 +88,70 @@ function clickyboy (i,j){
                 swap("main"+i+j,"main"+i+(j-1));
             }
         }
-    }   
+    } 
+    moves();
+    setTimeout(check,250);  
+}
+
+let counter,move=0;
+let a=[0,0,0];
+let Timer;
+let b=["","",""];
+
+//no of moves made functions
+function moves(){
+    document.getElementById("moves").innerHTML="No. of moves made: "+move;
+  }
+//timer
+function start() {
+      Timer = setInterval(function(){
+          a[2]+= 1;
+          if(a[2]==60){
+              a[1]++;
+              a[2]-=60;
+          }
+          if(a[1]==60){
+              a[0]++;
+              a[1]-=60;
+          }
+          for(let i=0;i<3;i++){
+              if((a[i]/10)<1){
+                  b[i]="0"+a[i];
+              }
+              else{
+                  b[i]=a[i];
+              }
+          }
+           document.getElementById("timer").innerText = "Time Elapsed: "+b[0]+":"+b[1]+":"+b[2];
+      }, 1000) ;
+  
+}
+
+  function resetTimer() {
+      a=[0,0,0];
+      document.getElementById("timer").innerText = "Time Elapsed: 00:00:00";
+      clearInterval(Timer);
+}
+
+//score function
+function scores(){
+    let points= 1000 - (((a[0]*60*60)+(a[1]*60)+(a[2]))*0.50)+(move*0.25);
+    return points;
+}
+function check(){
+    counter=0;
+    for(let i=1;i<=3;i++){
+        for(j=1;j<=3;j++){
+            let x=i-1;
+            let y=j-1;
+            if(document.getElementById("blah"+x+y).style.backgroundColor==document.getElementById("main"+i+j).style.backgroundColor)
+                counter++;
+        }
+        if(counter==9){
+            let s = scores();
+            console.log(s);
+            alert("You Won\nScore: "+s+" points\nThe higher the score, the better you have played");
+            resetTimer();
+        }
+    }
 }
